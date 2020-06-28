96b2d51208c053821fd32f2fe58a49dd59e9e029
achiratch <achiratch.cmp@gmail.com>
2020-06-28 17:12:55 +0700

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,51 +1,66 @@
-import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
-import moment from "moment";
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import moment from 'moment';
 
-export default class Dishdetail extends Component {
-  render() {
-    if (Object.keys(this.props).length > 0) {
-      const comments = this.props.comments.map((comment) => {
-        return (
-          <div align="left" key={comment.id}>
-            <div>{comment.comment}</div>
-            <div>
-              {"-- " +
-                comment.author +
-                ", " +
-                moment(comment.date).format("MMM DD, YYYY")}
-            </div>
-            <br />
-          </div>
-        );
-      });
+// Dishdetail is implemented as a functional (stateless) component
+class DishDetail extends Component {
 
+  renderDish(dish) {
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    )
+  }
+
+  renderComments(dish) {
+
+    const comments = dish.comments.map((comment) => {
       return (
-        <div className="row">
-          <div className="col-12 col-md-5 m-1 col-xs-12 col-sm-12">
-            <Card align="left">
-              <CardImg top src={this.props.image} alt={this.props.name} />
-              <CardBody>
-                <CardTitle>{this.props.name}</CardTitle>
-                <CardText>{this.props.description}</CardText>
-              </CardBody>
-            </Card>
+        <div key={comment.id}>
+          <div>{comment.comment}</div>
+          <div>
+              {'-- ' + comment.author + ', ' + moment(comment.date).format('MMM DD, YYYY')}
           </div>
-          <div className="col-12 col-md-5 m-1 col-xs-12 col-sm-12">
-            <h4 align="left">Comments</h4>
-            {comments}
+          <br/>
+        </div>
+      );
+    })
+
+    return (
+        <div>
+            <h3>Comments</h3>
+            <ul className="list-unstyled">
+                {comments}
+            </ul>
+        </div>
+    );
+  }
+
+  render() {
+    if (this.props.dish) {
+      return(
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1 col-xs-12 col-sm-12">
+              {this.renderDish(this.props.dish)}
+            </div>
+            <div className="col-12 col-md-5 m-1 col-xs-12 col-sm-12">
+                {this.renderComments(this.props.dish)}
+            </div>
           </div>
         </div>
       );
-    } else {
-      return <div></div>;
+    } else { // Empty props
+      return(
+        <div className="col-12"></div>
+      )
     }
   }
 }
+
+export default DishDetail;
